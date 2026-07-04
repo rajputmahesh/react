0c5c4c54a664495cc0b50fc8c077f924a6220479
Mahesh Rajput <mahesh.rajput3911@gmail.com>
2026-07-04 11:47:42 +0530

    syscfusion richedit with css formatting

--- docx-template-editor/src/App.tsx
+++ docx-template-editor/src/App.tsx
@@ -1,15 +1,13 @@
 
-/// <reference types="react" />
 import React, { useRef } from "react";
 // Ensure the automatic JSX runtime types are available
-import 'react/jsx-runtime';
 import "./App.css";
 import { registerLicense } from "@syncfusion/ej2-base";
 import { DocumentEditorContainerComponent, Toolbar, Ribbon } from "@syncfusion/ej2-react-documenteditor";
 
 DocumentEditorContainerComponent.Inject(Toolbar, Ribbon);
 
-registerLicense("YOUR LICENSE KEY HERE");
+registerLicense("");
     
 function App() {
   const container = useRef<DocumentEditorContainerComponent>(null);
